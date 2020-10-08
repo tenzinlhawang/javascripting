1c0c2112fd22126d692fa9a197cf961224787615
tenzinlhawang <tenzin.10zin@hotmaill.com>
2020-10-08 19:44:36 +0000

    Garbage management

---
+++ javascripting/smartGarbage.js
@@ -0,0 +1,10 @@
+const smartGarbage = function (trash, bins) {
+  if (trash === "waste") {
+    bins.waste += 1;
+  } else if (trash === "recycling") {
+      bins.recycling += 1;
+  } else if (trash === "compost") {
+    bins.compost += 1;
+  }
+  return bins;
+};
